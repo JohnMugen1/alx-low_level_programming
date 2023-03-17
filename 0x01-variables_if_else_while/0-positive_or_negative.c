24932d3132ff795818f88c16ccee0e3f00015408
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-17 10:55:49 -0700

    Determine if a number is positive,zero,or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main-Program to determin the status of a number
- * Read: 0(success)
+ * main - Determine whether a number is positive,zero ,or negat
+ * ve
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

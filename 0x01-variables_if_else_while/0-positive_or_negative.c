c1154cdbef96a6b98d1ae0d2ab5050e910900f4b
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-17 08:16:11 -0700

    C program that determines the status of a random value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,23 +10,26 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
+	/* output the random number */
+	printf("%d", n);
 	/*
 	 * Checking the value n using if..else..statement
 	 * If value n is greater than zero,then it is positive
 	 */
 	if (n > 0)
 	{
-		printf("%d is a positive number", n);
+		printf("is positive");
 	}
 	/* When value n is less than 0,then it is negative number */
 	else if (n < 0)
 	{
-		printf("%d is a negative number.", n);
+		printf("is negative");
 	}
 	/* Otherwise the value n is zero */
 	else
 	{
-		printf("The value is zero.");
+		printf("is zero");
 	}
 	return (0);
 }

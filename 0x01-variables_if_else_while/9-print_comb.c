45ad723af025f56249685daaea65f1392b348ce2
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-19 06:57:37 -0700

    Print the alphabet in lowercase

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,14 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num == 9)
+		{
+			putchar('$');
+		}
+		else
+		{
+			putchar((',')'+'(' '));
+		};
 	}
 	putchar('\n');
 	return (0);

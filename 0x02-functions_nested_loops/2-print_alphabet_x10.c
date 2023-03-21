6964b8c86b1361e202269aacaebd346b34dba1e0
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-21 12:26:34 -0700

    Print all alphabets 10 times in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,4 @@
 #include "main.h"
-
-/* function prototype */
-void print_alphabet_x10(void);
-
-/**
- * main -Print all alphabets 10 times in lowercase
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	/* function invokation */
-	print_alphabet_x10();
-	return (0);
-}
 /**
  * print_alphabet_x10 - prints 10 times the alphabet,in lowercase
  * followed by new line

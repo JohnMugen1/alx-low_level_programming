41cfc42c702e79571eaa666b31ca6667897d0081
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-23 11:22:01 -0700

    Draw a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,33 +1,16 @@
 #include "main.h"
 /**
- * print_line - Draw a straight line in the terminal
- * @n: n is the number of times to be printed
- * Return: new line if n is greater than 0, otherwise a new line
+ * print_line - Draws a straight line in the terminal
+ * Return: A joined dashes forming a line
  */
 void print_line(int n)
 {
-	/* checking if number in is less than 0 or not */
-	if (n < 0)
-	{
-		_putchar(' ');
-		return;
-	}
 	/* local variable declaration */
-	int i, j;
+	int i;
 
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			if (i == j)
-			{
-				_putchar('\\');
-			}
-			else
-			{
-				_putchar('\n');
-			}
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }

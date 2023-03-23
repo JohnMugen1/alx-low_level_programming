42351e42ac91eae58fa680eed737b434446c491c
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-23 12:50:38 -0700

    Print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - print square, followed by a line
+ * @size: is the number of times to print the symbols
+ * Return: a new line if n is less than 0,otherwise a square of hash symbol
+ */
+void print_square(int size)
+{
+	/* local variable declaration */
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}

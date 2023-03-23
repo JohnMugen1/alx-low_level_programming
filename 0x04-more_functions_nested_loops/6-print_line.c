53ec461314b965534b40001db823f655a63ba18a
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-23 11:42:41 -0700

    Draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,22 @@
 #include "main.h"
 /**
- * print_line - Draws a straight line in the terminal
- * Return: A joined dashes forming a line
+ * print_line - Draw  straight line in the terminal
+ * @n: the number of times to print the underscore
+ * Return: joined underscores forming a straight line
  */
 void print_line(int n)
 {
-	/* local variable declaration */
-	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	/* local variable declaration */
+		int i;
+
+		/* for loop */
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 }

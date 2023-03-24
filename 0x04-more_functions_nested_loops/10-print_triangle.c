f9de7da2d893d2312a50faf24ca99234160a40ab
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-23 22:06:17 -0700

    Prints a triangle using a hash symbol

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle follwed by a new line
+ * @size: the number of times to print the symbol
+ * Return: a triangle image formed by hash symbols
+ */
+void print_triangle(int size)
+{
+	/* local variable declaration */
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = size; j >= 0; j--)
+		{
+			if (j <= i)
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

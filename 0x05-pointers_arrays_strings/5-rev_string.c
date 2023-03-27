fb56454cc6368faf8829d0e6f69bfaec0317f024
JohnMugen1 <johnmugendik776@gmail.com>
2023-03-27 13:17:48 -0700

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - Reverses a string
+ * @s: - string pointer parameter
+ */
+void rev_string(char *s)
+{
+	int i, length;
+
+	length = strlen(s);
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
